173401b2a430ee2009bc97a169379108a6ad2f96
910980941@qq.com <910980941@qq.com>
2019-09-18 21:28:02 +0800

    系统日志异步记录

---
+++ Syslog-Data/src/main/java/timing/ukulele/data/syslog/LogExchange.java
@@ -0,0 +1,6 @@
+package timing.ukulele.data.syslog;
+
+public final class LogExchange {
+    public static final String SYS_LOG_EXCHANGE = "sys-log-exchange";
+    public static final String SYS_LOG_ROUTING_KEY = "sys.log";
+}
